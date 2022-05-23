2b37f7f595b89bea7eaff2b8b7142cb2fdd38da1
Hex Falc The Porter <84611854+Hexfalc@users.noreply.github.com>
2022-05-23 07:36:04 +0200

    update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -1741,9 +1741,9 @@ class FunkinLua {
 		});
 		
 		call('onCreate', []);
-	}
+	});
 
-inline static function getTextObject(name:String):FlxText;
+inline static function getTextObject(name:String):FlxText
 	{
 		return PlayState.instance.modchartTexts.exists(name) ? PlayState.instance.modchartTexts.get(name) : Reflect.getProperty(PlayState.instance, name);
 	}
